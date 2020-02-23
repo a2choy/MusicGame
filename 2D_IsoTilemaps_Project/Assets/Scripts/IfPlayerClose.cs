237ddbc5851a6977c894911605702fe9a629f1d7
a2choy <austinchoy84@gmail.com>
2020-02-22 20:00:43 -0800

    Added base class
    when player goes near the object the options panel shows up (goes away when moving away)
    
    you can click on the 3 things to change states (doesnt do anything except change state from 0,1,2)
    
    it is under prefabs frozenmountain/ swordinstone
    
    the thing is testable under scenes/examples/scene_biome_basic

---
+++ 2D_IsoTilemaps_Project/Assets/Scripts/IfPlayerClose.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfPlayerClose : MonoBehaviour
+{
+    public GameObject options;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Player")
+        {
+            options.SetActive(true);
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            options.SetActive(false);
+        }
+    }
+}
---
+++ 2D_IsoTilemaps_Project/Assets/Scripts/Interactable.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public GameObject options;
+    [SerializeField] int state = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            wp = new Vector3(wp.x, wp.y, 0);
+
+            CircleCollider2D colRed = options.transform.Find("ColRed").gameObject.GetComponent<CircleCollider2D>();
+            CircleCollider2D colYellow = options.transform.Find("ColYellow").gameObject.GetComponent<CircleCollider2D>();
+            CircleCollider2D colGreen = options.transform.Find("ColGreen").gameObject.GetComponent<CircleCollider2D>();
+
+            if (colRed.bounds.Contains(wp))
+            {
+                state = 0;
+            }
+            else if (colYellow.bounds.Contains(wp))
+            {
+                state = 1;
+            }
+            else if (colGreen.bounds.Contains(wp))
+            {
+                state = 2;
+            }
+        }
+        switch (state)
+        {
+            case 0:
+                //do sth
+                break;
+            case 1:
+                //do sth
+                break;
+            case 2:
+                //do sth
+                break;
+            default:
+                break;
+        }
+    }
+}

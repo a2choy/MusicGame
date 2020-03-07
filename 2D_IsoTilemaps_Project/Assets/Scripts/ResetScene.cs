e3f17ed253f5610234dc951581f44b5201b40f35
Matthew Engurasoff <menguras@ucsd.edu>
2020-03-06 16:41:20 -0800

    Added Basic UI for pause menu
    Reset button and volume slider

---
+++ 2D_IsoTilemaps_Project/Assets/Scripts/ResetScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ResetScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
